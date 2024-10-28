e98b80d0937b5918c05b84fe2b12e7e116732dea
Aritro Sinha Roy <ari.sinharoy@gmail.com>
2024-10-28 11:57:32 -0400

    Main commit
    uploading Python scripts containing the new DEER kernel expression, Tikhonov regularization method used in this work and DEERLab analysis script along with the experimental DEER data

---
+++ Code/deerlab_pr_reconstruction.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 16:12:44 2024
+
+@author: as836
+"""
+
+import deerlab as dl
+import numpy as np
+import os
+
+# mother directory
+_mpath = r'C:\Users\as836\Documents\GitHub\Rapid Simulator DEER'
+
+# write data
+_wpath = os.path.join(_mpath,'Data\DEERLab_Analysis')
+
+# read data
+_dpaths = ['Data\DEER Data_biradical-I',
+           'Data\DEER Data_biradical-II',
+           'Data\DEER Data_biradical-III']
+
+# molecular tags associated with the files
+_mtags = ['HQ', 'AF', 'AQ']
+
+# DEER data recorded with observation-pump frequency separation of
+_seps = [[20, 25, 30, 50, 70, 100],
+        [20, 25, 30, 50, 70, 100],
+        [20, 50, 70, 100]]
+
+# set the distance domain
+_rmins = [1.0, 1.5, 2.0]
+_rmaxs = [3.0, 3.5, 4.5]
+_delrs = [0.015, 0.01, 0.015]
+
+# set pulse separation time parameters
+_tmin = 0.28
+_tau1 = 0.40 
+_tau2s = [1.80, 2.00, 4.00]
+
+# run deerlab to get P(r) and fit background
+for i in range(3):
+    _dpath = os.path.join(_mpath,_dpaths[i])
+    _mtag = _mtags[i]
+    _rmin, _rmax, _delr = _rmins[i], _rmaxs[i], _delrs[i] 
+    _tau2 = _tau2s[i]
+
+    _jrange = len(_seps[i])
+    for j in range(_jrange):
+        
+        _fname = _mtag+'_'+str(_seps[i][j])+'MHz.DTA'
+        _t, _Vexp = dl.deerload(os.path.join(_dpath, _fname))
+
+        # pre-processing
+        _Vexp = dl.correctphase(_Vexp) # Phase correction
+        _Vexp = _Vexp/np.max(_Vexp)     # Rescaling (aesthetic)
+        _t = _t - _t[0]                 # Account for zerotime
+        _t = _t + _tmin
+    
+        # distance vector
+        _r = np.arange(_rmin, _rmax, _delr) # nm
+
+        # construct the model
+        _Vmodel = dl.dipolarmodel(_t, _r, 
+                                  experiment = dl.ex_4pdeer(_tau1, _tau2, 
+                                                            pathways = [1]))
+        
+        _compactness = dl.dipolarpenalty(Pmodel = None, r = _r, type = 'compactness')
+
+        # fit the model to the data
+        _results = dl.fit(_Vmodel, _Vexp, penalties = _compactness)
+
+        # background correction
+        _Bfcn = lambda mod,conc,reftime: _results.P_scale*(1-mod)*dl.bg_hom3d(
+            _t-reftime,conc,mod)
+        _Bfit = _results.evaluate(_Bfcn)
+        _Vcorr = 1 - _results.mod + _Vexp - _Bfit
+
+        # obtain P(r) 
+        _Pr = _results.P
+    
+        # save the data
+        np.savetxt(os.path.join(_wpath, _mtag+'_'+str(_seps[i][j])+'MHz_Pr.txt'), 
+                   np.c_[_r, _Pr])
+        np.savetxt(os.path.join(_wpath, _mtag+'_'+str(_seps[i][j])+'MHz_dlfit.txt'), 
+                   np.c_[_t, _Vexp, _Bfit])
---
+++ Code/pr_derivation.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 28 11:02:38 2024
+
+@author: as836
+"""
+
+import os
+import numpy as np
+from deer_newkernel import *
+from scipy.interpolate import interp1d 
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import normalize
+from scipy.optimize import nnls, lsq_linear
+import deerlab as dl
+import time
+
+def scmm(data):
+    
+    return (data - data[-1]) / (data.max() - data[-1])
+
+def movav(data, n=3):
+    _ndat = np.pad(data, (int((n-1)/2), int((n-1)/2)), 'constant', constant_values = (data[0], data[-1]))
+    return np.convolve(_ndat, np.ones(n)/n, mode='valid')
+
+def ptkn_deriv(X, Y, alpha):
+    n = X.shape[1]
+    L = np.identity(n)
+
+    A1 = np.concatenate((X, alpha*L))
+    b1 = np.concatenate((Y, np.zeros(n)))
+
+    theta, rnorm = nnls(A1, b1)
+
+    rho = np.linalg.norm(Y - np.dot(X, theta))
+    eta = np.linalg.norm(np.dot(np.identity(n), theta))
+
+    return theta, rnorm, rho, eta
+
+def fftdl(data, tdat, op = 2):
+    _nu, _spec = dl.fftspec(data - data.mean(), tdat)
+    
+    if op == 1:
+        return _nu, _spec
+    else:
+        return _spec
+
+# set path for fetching experimental data 
+_epath = r'C:\Users\as836\Documents\GitHub\Rapid Simulator DEER\Data\DEERLab_Analysis'
+_sep = 100 # observation and pump pulse separation in MHz
+_mol = 'AQ'
+_tag = _mol+'_'+str(_sep)+'MHz'
+_expt = np.loadtxt(os.path.join(_epath, _tag+'_dlfit.txt'))
+
+
+## pre-processing experimental data
+_pos = np.where(_expt[:,1] == _expt[:,1].max())[0][0]
+_expt = _expt[_pos:]
+_expt[:,0] -= _expt[0,0]
+
+_start = time.time()
+
+# define distance domain
+_rrange = np.arange(2.0, 4.5, 0.015)
+
+# define P(theta)
+_thtab = np.linspace(0, np.pi/2, 100)
+_pth = np.sin(_thtab)
+_pth /= _pth.sum()
+
+# define resonance offset range with a gaussian probability distribution
+_wid = 4  # 4 for tp>=50 ns, 5 for tp=40 ns, 6 for the rest
+_mult = 4
+_omtab = np.linspace(-_mult*_wid, _mult*_wid, 100)
+_pom = np.exp(-_omtab**2/(2*_wid**2))
+_pom /= _pom.sum()
+
+
+_omtab2 = np.linspace(-_mult*_wid - _sep, _mult*_wid - _sep, 100)
+_pom2 = np.exp(-(_omtab2 + _sep)**2/(2*_wid**2))
+_pom2 /= _pom2.sum()
+
+# define pi-pulse magnetic field and duration
+_wp2, _wp = 10.00, 10.00 # pulse field in rad.MHz
+_tp2, _tp = 25.*1E-3, 50.*1E-3 # pulse length in us
+
+# define time-domain parameters
+_tau1 = 0.4
+_tau2 = 4.0
+_ttab = _expt[:,0].copy()
+
+_tdat, _rdat = np.meshgrid(_ttab, _rrange, indexing = "ij")
+_tdat, _rdat = np.ndarray.flatten(_tdat), np.ndarray.flatten(_rdat)
+
+# set maximum number of iterations
+_mmax = 5*1E2
+
+# calculate the design matrix
+_res = np.zeros((_ttab.shape[0], _rrange.shape[0]))
+_m = 1
+while _m <= _mmax:
+    _om1, _om2 = np.random.choice(_omtab, p = _pom), np.random.choice(_omtab2, p = _pom2)
+    
+    if _om1==_om2:
+        _om2 += 1E-4
+    _th = np.random.choice(_thtab, p = _pth)
+    
+    _dip = 2*np.pi*52.04/_rdat**3 * (1-3*np.cos(_th)**2)
+
+    _sig1 = deer4p(_om1, _om2, _dip, _wp2, _tp2, _wp, _tp, _sep, 
+                   _tau1, _tau2, _tdat)
+    
+    _res -= _sig1.reshape(_ttab.shape[0], _rrange.shape[0])
+    _m += 1
+    
+_res /= int(_mmax)
+
+_end = time.time() - _start
+
+print("SimuLation with %sk iterations took %s sec." %(int(_mmax*1E-3), np.round(_end,2)))
+
+# calculate P(r) using Tikhonov regularization
+_datx1 = _res.copy()
+_dim = _datx1.shape[1]
+_dat1 = np.array([scmm(_datx1[:,x]) for x in range(_dim)]).T
+
+# calculate the P(r) from the time-domain data
+_daty1 = scmm(_expt[:,1] - _expt[:,2])
+_ptkn1 = ptkn_deriv(_dat1, _daty1, 1)[0] # regularization parameter lambda is set to 1
+
+# calculate kernel matrix in the frequency domain
+_datf1 = np.array([scmm(fftdl(_dat1[:,x], _expt[:,0])) for x in range(_dim)]).T
+
+# calculate FFT of the experimental data
+_fdat, _daty2 = fftdl(_expt[:,1] - _expt[:,2], _expt[:,0], op = 1)
+_daty2 = scmm(_daty2)
+
+# calculate the P(r)
+_ptkn2 = ptkn_deriv(_datf1, _daty2, 1)[0] # regularization parameter lambda is set to 1
+
+# plot the averaged P(r)
+plt.plot(_rrange, scmm(movav(_ptkn1*_ptkn2, 3)), 'b')
+plt.show()
+
+# save the data
+_wpath = r'C:\Users\as836\Documents\GitHub\Rapid Simulator DEER\Data\NewKernel_Pr'
+np.savetxt(os.path.join(_wpath, _tag+'_Pr.txt'), np.c_[_rrange, scmm(movav(_ptkn1*_ptkn2, 3))])
